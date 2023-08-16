9982a1700f496899e0524e016130e274b7f992ff
DasSalz <g9191hc@gmail.com>
2023-08-16 18:00:13 +0900

    start

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:widget_sample/layout/default_layout.dart';
+
+void main() {
+  runApp(_App());
+}
+
+class _App extends StatelessWidget {
+  const _App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+
+    );
+  }
+}
---
+++ lib/widget/snack_bar.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:widget_sample/layout/default_layout.dart';
+
+class SnackBarSample extends StatelessWidget {
+  const SnackBarSample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(child: child);
+  }
+}
