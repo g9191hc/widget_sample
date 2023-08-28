a12dbf1d7ef7fe15140c8a6aff44446e6a55fb3a
IG <g9191hc@gmail.com>
2023-08-28 20:55:17 +0900

    feat: [add]  문제별 Splash Screen

---
+++ lib/data/data.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+final PRIMARY_COLOR_B = Color(0xff1E2E6F);
+final PRIMARY_COLOR_R = Color(0xffD75A4B);
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:widget_sample/layout/default_layout.dart';
+import 'package:widget_sample/layout/splash_screen.dart';
 import 'package:widget_sample/widget/widget_list.dart';
 import 'package:widget_sample/widget/snack_bar.dart';
 import 'package:dcdg/dcdg.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
-    await SentryFlutter.init(
-          (options) {
-        options.dsn = 'https://3bb8ee0d705b0b9c7b9df3939060eaaa@o4505777137319936.ingest.sentry.io/4505777196171264';
-        // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-        // We recommend adjusting this value in production.
-        options.tracesSampleRate = 1.0;
-      },
-      appRunner: () => runApp(const _App()),
-    );
-    // or define SENTRY_DSN via Dart environment variable (--dart-define)
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          'https://3bb8ee0d705b0b9c7b9df3939060eaaa@o4505777137319936.ingest.sentry.io/4505777196171264';
+      options.tracesSampleRate = 0.01;
+    },
+    appRunner: () => runApp(const _App()),
+  );
 }
 
 class _App extends StatelessWidget {
